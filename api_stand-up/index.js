15cf443163e399c7377b3a9f39689d163c2c6942
Deolys <ed03aleks@mail.ru>
2023-12-03 13:08:17 +0300

    commit 1

---
+++ api_stand-up/index.js
@@ -0,0 +1,77 @@
+import http from "node:http";
+import fs from "node:fs/promises";
+import { sendData, sendError } from "./modules/send.js";
+import { checkFile } from "./modules/checkFile.js";
+import { handleComediansRequest } from "./modules/handleComediansRequest.js";
+import { handleAddClient } from "./modules/handleAddClient.js";
+import { handleClientsRequest } from "./modules/handleClientsRequest.js";
+import { handleUpdateClient } from "./modules/handleUpdateClient.js";
+
+const PORT = 8080;
+const COMEDIANS = './comedians.json';
+export const CLIENTS = './clients.json';
+
+
+const startServer = async () => {
+ if (!(await checkFile(COMEDIANS))) {
+  return;
+ }
+
+ await checkFile(CLIENTS, true);
+
+  const comediansData = await fs.readFile(COMEDIANS, 'utf-8');
+  const comedians = JSON.parse(comediansData);
+
+  http.createServer(async (req, res) => {
+    try {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, OPTIONS");
+      res.setHeader("Access-control-Allow-HEaders", 'Content-Type');
+
+      if(req.method === 'OPTIONS') {
+        res.writeHead(204);
+        res.end();
+        return;
+      }
+
+      const segments = req.url.split('/').filter(Boolean);/*Фишка, которая убирает пустые значения*/
+
+    if(req.method === "GET" && segments[0] === 'comedians') {
+      handleComediansRequest(req, res, comedians, segments);
+      return;
+
+    } 
+    
+    if(req.method === "POST" && segments[0] === 'clients') {
+      handleAddClient(req, res);
+      return;
+      //POST /clients
+      //Добавление клиента
+    }
+    
+    if(req.method === "GET" && segments[0] === 'clients' && segments.length === 2) {
+      const ticketNumber = segments[1];
+      handleClientsRequest(req, res, ticketNumber);
+      return;
+      //GET /clients/:ticket
+      //Получение клиента по номеру билета
+    }
+
+    if(req.method === "PATCH" && segments[0] === 'clients' && segments.length === 2) {
+      handleUpdateClient(req, res, segments);
+      return;
+      //PATCH /clients/:ticket
+      //Обновление клиента по номеру билета
+    }
+    sendError(res, 404, 'Not found');
+    } catch (error) {
+      sendError(res, 500, `Ошибка сервера: ${error}`);
+    }
+    
+  }).listen(PORT);
+  
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
+};
+
+startServer();
+
---
+++ standup/scripts/form.js
@@ -0,0 +1,110 @@
+import Inputmask from 'inputmask';
+import JustValidate from 'just-validate';
+import { Notification } from './notification';
+
+export const initForm = (bookingForm, bookingInputFullname, bookingInputPhone, bookingInputTicket, ChangeSection, bookingComediansList) => {
+  const validate = new JustValidate(bookingForm, {
+    errorFieldCssClass: 'booking__input_invalid',
+    successFieldCssClass: 'booking__input_valid',
+  });
+
+  new Inputmask('+7(999)-999-99-99').mask(bookingInputPhone);
+  new Inputmask('99999999').mask(bookingInputTicket);
+
+  validate
+  .addField(bookingInputFullname, [{
+    rule: 'required',
+    errorMessage: 'Заполните имя'
+  },
+  ])
+  .addField(bookingInputPhone, [{
+    rule: 'required',
+    errorMessage: 'Заполните телефон'
+  },
+  {
+    validator() {
+      const phone = bookingInputPhone.inputmask.unmaskedvalue();
+      return phone.length === 10 && !!Number(ticket);
+    },
+    errorMessage: 'Некорректный телефон',
+  },
+  ])
+  .addField(bookingInputTicket, [{
+    rule: 'required',
+    errorMessage: 'Заполните номер билета',
+  },
+  {
+    validator() {
+      const ticket = bookingInputTicket.inputmask.unmaskedvalue();
+      return ticket.length === 8 && !!Number(ticket);
+    },
+    errorMessage: 'Неверный номер билета',
+  },
+  ]).onFail((fields) => {
+    let errorMessage = '';
+    for (const key in fields) {
+      if (!Object.hasOwnProperty.call(fields, key)) {
+        continue;
+      }
+
+      const element = fields[key];
+      if (!element.isValid) {
+        errorMessage += `${element.errorMessage}, `;
+      }
+    }
+
+    Notification.getInstance().show(errorMessage.slice(0, -2), false);
+  });
+
+  bookingForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    if(!validate.isValid) {
+      return;
+    }
+
+    const data = {booking: []};
+    const times = new Set();
+
+    new FormData(bookingForm).forEach((value, field) => {
+      if (field === 'booking') {
+        const [comedian, time] = value.split(',');
+
+        if (comedian && time) {
+          data.booking.push({comedian, time});
+          times.add(time);
+        }
+
+      } else {
+        data[field] = value;
+      }
+    });
+
+    if(times.size !== data.booking.length) {
+      Notification.getInstance().show('Нельзя быть в одно время на двух выступлениях', false);
+      return;
+    }
+    
+    if(!times.size) {
+      Notification.getInstance().show('Вы не выбрали комика и/или время');
+      return;
+    }
+
+    const method = bookingForm.getAttribute('method');
+
+    let isSend = false;
+    if(method === 'PATCH') {
+      isSend = await sendData(method, data, data.ticketNumber);
+    } else {
+      isSend = await sendData(method, data);
+    }
+
+    if(isSend) {
+       Notification.getInstance().show('Бронь принята', true);
+       ChangeSection();
+       bookingForm.reset();
+       bookingComediansList.textContent = "";
+    }
+   
+  });
+} 
